354f9e1fe6c3e72bc9bfe4ff3f298ceda0e3d264
Xtatolum <44492797+Xtatolum@users.noreply.github.com>
2019-11-10 12:23:16 +0100

    Classe form Inscription

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use OC\PlatformBundle\Entity\Advert;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class InscriptionController extends Controller
+{
+  public function addAction(Request $request)
+  {
+    // On crée un objet Personne
+    $personne = new Personne();
+
+    // On crée le FormBuilder grâce au service form factory
+    $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $personne);
+
+    // On ajoute les champs de l'entité que l'on veut à notre formulaire
+    $formBuilder
+      ->add('identifiant',      TextType::class)
+      ->add('nom',     TextType::class)
+      ->add('prenom',   TextType::class)
+      ->add('localisation',    TextType::class)
+      ->add('campus', TextType::class)
+	  ->add('adresse email', EmailType::class)
+	  ->add('mot de passe', PasswordType::class)
+      ->add('valider',      SubmitType::class)
+    ;
+
+    // À partir du formBuilder, on génère le formulaire
+    $form = $formBuilder->getForm();
+
+    // On passe la méthode createView() du formulaire à la vue
+    // afin qu'elle puisse afficher le formulaire toute seule
+    return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+      'form' => $form->createView(),
+    ));
+  }
+}
