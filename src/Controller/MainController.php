8868ce70196eff88f7b23f830a152ce988073fd0
SazuChan <kevin.hoang@viacesi.fr>
2019-11-15 09:27:58 +0100

    Connexion fonctionnelle

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -14,9 +15,15 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpClient\CurlHttpClient;
 use App\Entity\Personnes;
 use App\Entity\Produits;
+use App\Entity\Activites;
 
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 class MainController extends AbstractController
 {
@@ -202,6 +209,9 @@ class MainController extends AbstractController
 		
 		//création d'un object personne vide
 		$personne = new Personnes();
+		$encoders = [new JsonEncoder()];
+		$normalizers = [new ObjectNormalizer()];
+		$serializer = new Serializer($normalizers, $encoders);
 		
 		//paramètre du formulaire relier aux attributs de l'object Personne
 		$form = $this->createFormBuilder($personne)
@@ -220,36 +230,50 @@ class MainController extends AbstractController
 		$personne->setMotdepasse(crypt($personne->getMotdepasse(), 'dkPOpjfiIsjni16/idjsdi:AZEIIjsdquIisdsji/1839'));	
 		
 		//dump = info dev		
-		var_dump($personne);
+		//var_dump($personne);
 		
 		//si le formulaire a été soumis et est valide
 		if($form->isSubmitted() && $form->isValid()) {
 					
 			//transforme en json les réponses du formulaire
-			$login['data']=json_encode($personne);
+			$data = $form->getData();
+			$json_data = $serializer->serialize($data, 'json');
+			
 			$url = 'http://localhost:3000/personnes?inscription=true';
 			
 			//ouverture de la connexion
 			$open_co=curl_init();
 			
+			$header = [
+				'Accept: application/json',
+				'Content-Type: application/json'
+			];
+
+			dump($json_data);
+
+
 			//configuration de l'envoie et envoie
-			curl_setopt($open_co,CURLOPT_URL,$url);
+			curl_setopt($open_co, CURLOPT_URL,$url );
 			curl_setopt($open_co, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($open_co,CURLOPT_POSTFIELDS,$login);
-			curl_setopt($open_co, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($open_co, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+			curl_setopt($open_co, CURLOPT_HTTPHEADER, $header);
+			curl_setopt($open_co, CURLOPT_POSTFIELDS, $json_data);
 			
+			curl_setopt($open_co, CURLOPT_RETURNTRANSFER, true);
+
 			//réponse
 			$return = curl_exec($open_co);
 			
 			//fermeture de la connection
 			curl_close($open_co);
 			
-			//décode le json 
-			$result = json_decode($return);
+
+			dump($return);
+			$result = json_decode($return, true);
 			
 			//si connected dans $result prends la valeur true on est inscrit sinon on n'est pas inscrit 
-			if($result['connected']==TRUE) 
+			
+			if($result['inscription'] == "Inscription reussie !") 
 			{
 				?>
 				<script>alert("inscription réussi !")</script>
@@ -263,9 +287,8 @@ class MainController extends AbstractController
 				?>
 				<script>alert("inscription echouée !")</script>
 				<?php
-				
-				echo $login['data'];
 			}
+			
 		}
 	
 		//envoie le formulaire pour le construire sur la page web
@@ -282,6 +305,75 @@ class MainController extends AbstractController
             'Title' => "Bonjour, bonjour"
 
 
+        ]);
+
+	}
+	
+	/**
+     *  @Route("/evenement/new"), name="addevenement")
+     */
+    public function addevenement(Request $request) {
+
+		$evenement = new Activites();
+		$encoders = [new JsonEncoder()];
+		$normalizers = [new ObjectNormalizer()];
+		$serializer = new Serializer($normalizers, $encoders);
+
+		$form = $this->createFormBuilder($evenement)
+					 ->add('description', TextType::class)
+					 ->add('image', TextType::class)
+					 ->add('cout', CheckboxType::class)
+					 ->add('recurrence', CheckboxType::class)
+					 ->add('valide', CheckboxType::class)
+					 ->getForm();
+
+		$form->handleRequest($request);
+
+		
+
+		if($form->isSubmitted() && $form->isValid()) {
+			$evenement->setDate(new \DateTime());
+			$data = $form->getData();
+			$json_data = $serializer->serialize($data, 'json');
+
+			$url = 'http://localhost:3000/activites';
+
+			$open_co=curl_init();
+
+			$header = [
+				'Accept: application/json',
+				'Content-Type: application/json'
+			];
+
+			dump($json_data);
+
+					
+			//configuration de l'envoie et envoie
+			curl_setopt($open_co, CURLOPT_URL,$url );
+			curl_setopt($open_co, CURLOPT_CUSTOMREQUEST, "POST");
+		
+			curl_setopt($open_co, CURLOPT_HTTPHEADER, $header);
+			curl_setopt($open_co, CURLOPT_POSTFIELDS, $json_data);						
+						
+			curl_setopt($open_co, CURLOPT_RETURNTRANSFER, true);
+			
+			$return = curl_exec($open_co);
+			
+			//fermeture de la connection
+			curl_close($open_co);
+
+			//décode le json 
+			$result = json_decode($return);
+
+			dump($result);
+						
+		}
+
+
+
+        return $this->render('main/addevenement.html.twig', [
+			'Title' => "Bonjour, bonjour",
+			'formAddEvent' => $form->createView()
         ]);
 
     }
